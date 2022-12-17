ff30832be0605bbc7f23fee3e46ec6ce70ae80f4
Eduardo Santizo <eddysanoli@gmail.com>
2022-12-17 16:20:11 -0600

    First Test Inserting a Custom Vue Component
    Decided to follow the Vue tutorials by "The Net Ninja" (https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=3&ab_channel=TheNetNinja). These videos explain how to create projects using the previous version of Vue (Vue 2). The new version (used by this project, since its the default version used and templated by Vite when creating a new project) its going to be explained later on in the video series, but for now Im trying to apply what I see in the first 3 video tutorials while I research how to make it work in the V3 environment that Im working on. For that I created a very simple Vue component that depends on "onClick" events.
    
    Other changes:
    - I updated the default node types. Previously I was using a deprecated version that was causing errors when running "npm run dev"

---
+++ website/frontend/src/components/Test.vue
@@ -0,0 +1,49 @@
+<template>
+    <hr>
+    <h3>This a test</h3>
+
+    <!-- Event listener -->
+    <button @click="toggleClicked">Toggle State: {{ "True" ? clicked : "False" }}</button>
+
+    <!-- Remove and Re-add to the DOM  -->
+    <div v-if="clicked">
+        <p>Hide this!</p>
+    </div>
+
+    <!-- Hide or show with CSS -->
+    <div class="elamigos" v-show="!clicked">
+        <p>Another secret!</p>
+    </div>
+
+    <hr>
+</template>
+
+<!-- Syntactic sugar for a regular script tag with -->
+<!-- a "setup()" function defined inside -->
+<script setup lang="ts">
+
+    import { Ref, ref } from 'vue'
+
+    // Reactive state
+    const clicked: Ref<boolean> = ref(false);
+
+    // Functions
+    const toggleClicked = () => {
+        clicked.value = !clicked.value;
+        console.log("CLICKED THIS SHIT");
+    }
+    
+</script>
+
+<style lang="scss">
+
+    .elamigos {
+       border: 1px solid black; 
+       border-radius: 10px;
+
+       p {
+        background-color: red;
+       }
+    }
+
+</style>
--- website/frontend/src/main.ts
+++ website/frontend/src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// Insert the "App" vue component into the div with ID "app".
+// Said div is located in the index.html file
+createApp(App).mount('#app');
