19782bf282a0c1e1b42a3e082652fd3b1f237c9f
Eduardo Santizo <eddysanoli@gmail.com>
2022-12-17 22:06:04 -0600

    Teleport
    Forgot to include the use of the "Teleport" tag, which allows you to place the teleported element into another root HTML element instead of inside the root "App".

--- website/frontend/src/components/Tutorial4.vue
+++ website/frontend/src/components/Tutorial4.vue
@@ -6,18 +6,21 @@
 
     <!-- We can apply modifiers to an event by doing "event.modifier" -->
     <button @click.left="{ showModal = !showModal; }">Show Modal (Left Click)</button>
-    <Modal
-        v-if="showModal"
-        title="Hello my brotha, this is tutorial 4"
-        @close="{ showModal = false; }"
-    > 
-        <p>😊</p>
-        <template v-slot:links>
-            <a href="https://google.com">Google</a>
-            <a href="https://youtube.com">Youtube</a>
-        </template>        
-        
-    </Modal>
+
+    <teleport to="#modals">
+        <Modal
+            v-if="showModal"
+            title="Hello my brotha, this is tutorial 4"
+            @close="{ showModal = false; }"
+        > 
+            <p>😊</p>
+            <template v-slot:links>
+                <a href="https://google.com">Google</a>
+                <a href="https://youtube.com">Youtube</a>
+            </template>        
+            
+        </Modal>
+    </teleport>
 
 </template>
 
